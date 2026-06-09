2153293de5fe54af81d65e5995b148caf2db0957
Suri Phan <phanoanh485@gmail.com>
2026-06-10 00:38:39 +0700

    feat: ex1 to ex9

---
+++ tests/lesson-02/ex4.js
@@ -0,0 +1,6 @@
+let high = 153;
+let idealweight = (high - 100)*9/10;
+let minweight = (high - 100)*8/10;
+let maxweight = high -100
+
+console.log("Cân nặng lý tưởng, tối đa, tối hiểu lần lược là:", idealweight, "kg,",maxweight,"kg,",minweight, "kg");
---
+++ tests/lesson-02/ex7.js
@@ -0,0 +1,6 @@
+console.log("Từ 1000 đến 2000 các số chia hết cho 3 là:")
+for (i = 1000; i <= 2000; i++){
+    if(i%3 === 0){
+        console.log(i)
+    }
+}
